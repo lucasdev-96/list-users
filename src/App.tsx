875ba6dae64a58733622abafa95f959de614a96c
lucasdev-96 <lucasgodoidev96@gmail.com>
2023-04-12 10:25:02 -0300

    feat: :sparkles: Create Home and User pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Home } from './Pages/Home';
+import { User } from './Pages/User';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: Home(),
+  },
+  {
+    path: "/user",
+    element: User()
+  }
+]);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <RouterProvider router={router} />
   );
 }
 
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,6 @@
+
+export const Home = () => {
+    return (
+        <div>Olá</div>
+    )
+}
