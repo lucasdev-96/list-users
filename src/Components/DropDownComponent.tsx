e8ec09da34ae218d03c8274996141985342b9c82
lucasdev-96 <lucasgodoidev96@gmail.com>
2023-04-13 15:41:29 -0300

    refactor: :recycle: edit dropdown

--- src/Components/DropDownComponent.tsx
+++ src/Components/DropDownComponent.tsx
@@ -3,17 +3,17 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import { UserContext } from '../Context/UserContextProvider';
 
 function DropDownComponent() {
-    const {setGender} = useContext(UserContext)
+    const {setGender, gender} = useContext(UserContext)
 
   return (
     <Dropdown >
       <Dropdown.Toggle variant="dark" id="dropdown-basic">
-        Gender
+        {gender ?  `${gender.charAt(0).toUpperCase()}${gender.slice(1)}` : 'Gender'}
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
         <Dropdown.Item onClick={() => setGender('male')}>Male</Dropdown.Item>
-        <Dropdown.Item>Female</Dropdown.Item>
+        <Dropdown.Item onClick={() => setGender('female')}>Female</Dropdown.Item>
       </Dropdown.Menu>
       
     </Dropdown>
