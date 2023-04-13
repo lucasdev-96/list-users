b8892d38d8f0dccbe5572a5d7e469cd6b836bb38
lucasdev-96 <lucasgodoidev96@gmail.com>
2023-04-13 15:41:10 -0300

    fix: :bug: edit api

--- src/Api/users.ts
+++ src/Api/users.ts
@@ -1,6 +1,5 @@
 import axios from "axios"
-import { info } from "console"
 
 export const getAllUsers = async (nat: string, gender: string): Promise<{results: Array<any>, info: any}> => {
-    return (await axios.get(`https://randomuser.me/api/?page=5&results=10&nat=${nat}&gender=${gender}`)).data
+    return (await axios.get(`https://randomuser.me/api/?results=200&nat=${nat}&gender=${gender}`)).data
 }
