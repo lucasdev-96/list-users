f23cea007293399a609e361197e2f57b1929101d
lucasdev-96 <lucasgodoidev96@gmail.com>
2023-04-13 16:37:38 -0300

    refactor: :recycle: table component

--- src/Components/TableComponent.tsx
+++ src/Components/TableComponent.tsx
@@ -1,17 +1,18 @@
 import { useContext } from 'react';
 import Table from 'react-bootstrap/Table';
 import { UserContext } from '../Context/UserContextProvider';
+import '../css/table.css'
 
 function TableComponent() {
-  const {users} = useContext(UserContext)
+  const {usersPagination} = useContext(UserContext)
   const returnUsers = () => {
-    return users ? users.map((value: any, index: any) => {
+    return usersPagination ? usersPagination.map((value: any, index: any) => {
       return (
         <tr key={index}>
           <td>{index + 1}</td>
           <td><img src={value.picture.thumbnail } alt='person image'/></td>
-          <td>{value.name.first}</td>
-          <td>{value.name.last}</td>
+          <td>{`${value.name.first} ${value.name.last}`}</td>
+          <td>{value.location.country}</td>
           <td>{value.login.username}</td>
           </tr>
 
@@ -19,20 +20,23 @@ function TableComponent() {
     }) : <tr><td>Users Not Found</td></tr>
   }
   return (
-    <Table responsive="lg"  striped bordered hover variant="dark">
+    <>
+    {usersPagination.length > 0 ? <Table responsive="lg"  striped bordered hover variant="dark">
       <thead>
         <tr>
           <th>#</th>
           <th>Picture</th>
-          <th>First Name</th>
-          <th>Last Name</th>
+          <th>Name</th>
+          <th>Country</th>
           <th>Username</th>
         </tr>
       </thead>
       <tbody>
         {returnUsers()}
       </tbody>
-    </Table>
+    </Table> : 'Loading...'}
+    
+    </>
   );
 }
 
