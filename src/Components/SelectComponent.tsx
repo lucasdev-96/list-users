1a4f9d04cb92a8bad1cf6f4b50d0bd35313fbfe1
lucasdev-96 <lucasgodoidev96@gmail.com>
2023-04-14 13:50:28 -0300

    feat: :bug: responsive select

--- src/Components/SelectComponent.tsx
+++ src/Components/SelectComponent.tsx
@@ -1,25 +1,22 @@
-import { countries } from '../utils/arrayCountrys';
+
 import { useContext } from 'react';
 import { UserContext } from '../Context/UserContextProvider';
+import { Select, CaretIcon, ModalCloseButton } from 'react-responsive-select';
+import { countries } from '../utils/arrayCountrys';
 import '../css/select.css'
 
 
 function SelectComponent() {
-    const value = Object.entries(countries)
-    const { setNat, nat } = useContext(UserContext)
-    
-    const render = () => { 
-            return value.map((value) => {
-            return (
-               <option key={value[0]} value={value[0]}>{value[1]}</option>
-            )
-        })
-    }
+    const { setNat } = useContext(UserContext)
   return (
     <>
-     <select className='selectCountry' value={nat} onChange={(e) => setNat(e.target.value)} >
-        {render()}
-    </select>
+      <Select
+      name="countries"
+      modalCloseButton={<ModalCloseButton />}
+      options={countries}
+      caretIcon={<CaretIcon />}
+      onChange={(newValue: any) => setNat(newValue.value)}
+    />
     </>
   );
 }
